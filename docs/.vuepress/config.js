8448760bfc8be1c781b4f88ebef8cadf009b7dcc
yisheng9 <yisheng9@vt.edu>
2020-01-11 16:13:29 -0500

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -101,7 +101,7 @@ module.exports = {
           },
           {
             title: '2. LAMMPS install',   // 必要的
-            path: '/note/1/',      // 可选的, 应该是一个绝对路径
+            path: '/note/2/',      // 可选的, 应该是一个绝对路径
             collapsable: true, // 可选的, 默认值是 true,
             sidebarDepth: 1,    // 可选的, 默认值是 1
             children: [
